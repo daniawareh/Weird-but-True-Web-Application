b863b8219c7f013f0eb4b309aa7c3479fb08d0ce
Alejandra Gonzalez <gonzalez.alejandra.013@gmail.com>
2017-11-06 13:08:12 -0800

    API controller to get specific fact

--- app_api/controllers/facts.js
+++ app_api/controllers/facts.js
@@ -6,22 +6,44 @@ var sendJsonResponse = function(res, status, content) {
     res.json(content);
 };
 
+/* Add a new fact to database. */
 module.exports.addFact = function (req, res) { 
     sendJsonResponse(res, 200, {"status" : "success"});
 };
 
+/* Get all facts from database. */
 module.exports.getAllFacts = function (req, res) { 
     sendJsonResponse(res, 200, {"status" : "success"});
 };
 
-module.exports.getFact = function (req, res) { 
-    sendJsonResponse(res, 200, {"status" : "success"});
+/* Get a specific fact from database. */
+module.exports.getFact = function (req, res) {
+    if (req.params && req.params.factid){
+        Fact.findById(req.params.factid).exec(
+            function(err, fact) {
+                if (!fact){
+                    sendJsonResponse(res, 404, {"message":"factid not found"});
+                    return;
+                }
+                else if (err){
+                    sendJsonResponse(res, 404, err);
+                    return;
+                }
+                sendJsonResponse(res, 200, fact); 
+            }
+        );
+    }
+    else {
+        sendJsonResponse(res, 404, {"message": "No factid in request"});
+    }
 };
 
+/* Edit a specific fact. */
 module.exports.editFact = function (req, res) { 
     sendJsonResponse(res, 200, {"status" : "success"});
 };
 
+/* Delete specific fact from database. */
 module.exports.deleteFact = function (req, res) { 
     sendJsonResponse(res, 200, {"status" : "success"});
 };
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var ctrlFacts = require('../controllers/facts');
 
 router.post('/facts', ctrlFacts.addFact); //create a new fact
-
 router.get('/facts', ctrlFacts.getAllFacts); //fetch a list of all facts
 router.get('/facts/:factid', ctrlFacts.getFact); //fetch a specific fact
 router.put('/facts/:factid', ctrlFacts.editFact); //update a specific fact
